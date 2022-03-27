9a12f47ecd4b4c89ec136262d07ef731a2785823
Jordan Rovny <rovnyjordan@gmail.com>
2022-03-26 22:23:29 -0500

    Modify Startup to use EF for datastores

--- src/JRovnySites.IdentityManagement/Startup.cs
+++ src/JRovnySites.IdentityManagement/Startup.cs
@@ -1,6 +1,8 @@
-﻿using IdentityServer4;
+﻿using System.Reflection;
+using IdentityServer4;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +13,7 @@ namespace JRovnySites.IdentityManagement
     {
         public IWebHostEnvironment Environment { get; }
         private readonly IConfiguration _configuration;
+        private readonly string _migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 
         public Startup(IWebHostEnvironment environment, IConfiguration configuration)
         {
@@ -22,14 +25,26 @@ namespace JRovnySites.IdentityManagement
         {
             services.AddControllersWithViews();
 
+            var connectionString = _configuration.GetConnectionString("Default");
+
+            if (connectionString == null)
+                throw new System.Exception("No connection string found");
+
             var builder = services.AddIdentityServer(options =>
             {
                 // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
                 options.EmitStaticAudienceClaim = true;
             })
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients);
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseNpgsql(connectionString,
+                    sql => sql.MigrationsAssembly(_migrationsAssembly));
+            })
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseNpgsql(connectionString,
+                    sql => sql.MigrationsAssembly(_migrationsAssembly));
+            });
 
             IConfigurationSection configurationSection =
                 _configuration.GetSection("ApplicationSettings").GetSection("Google");
