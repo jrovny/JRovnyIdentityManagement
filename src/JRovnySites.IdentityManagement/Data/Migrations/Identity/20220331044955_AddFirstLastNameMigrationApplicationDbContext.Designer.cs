86fda64e383268bb982b609e0d0c3a52192e2c72
Jordan Rovny <rovnyjordan@gmail.com>
2022-03-31 22:07:11 -0500

    Add EF migration for first and last names

--- src/JRovnySites.IdentityManagement/Data/Migrations/Identity/20220331044955_AddFirstLastNameMigrationApplicationDbContext.Designer.cs
+++ src/JRovnySites.IdentityManagement/Data/Migrations/Identity/20220331044955_AddFirstLastNameMigrationApplicationDbContext.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using JRovnySites.IdentityManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace JRovnySites.IdentityManagement.Data.Migrations.Identity
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220331044955_AddFirstLastNameMigrationApplicationDbContext")]
+    partial class AddFirstLastNameMigrationApplicationDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/JRovnySites.IdentityManagement/Data/Migrations/Identity/20220331044955_AddFirstLastNameMigrationApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JRovnySites.IdentityManagement.Data.Migrations.Identity
+{
+    public partial class AddFirstLastNameMigrationApplicationDbContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "first_name",
+                table: "identity_user",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "last_name",
+                table: "identity_user",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "first_name",
+                table: "identity_user");
+
+            migrationBuilder.DropColumn(
+                name: "last_name",
+                table: "identity_user");
+        }
+    }
+}
