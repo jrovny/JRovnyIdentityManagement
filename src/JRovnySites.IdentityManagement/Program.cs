539628e7789f3a8b8966096a863e454edcf58908
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-04 23:54:02 -0500

    Add rolling log file

--- src/JRovnySites.IdentityManagement/Program.cs
+++ src/JRovnySites.IdentityManagement/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using Serilog.Formatting.Json;
 using Serilog.Sinks.SystemConsole.Themes;
 using System;
 
@@ -47,7 +48,20 @@ namespace JRovnySites.IdentityManagement
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog()
+                .UseSerilog((context, logger) =>
+                {
+                    var isDevelopment = context.HostingEnvironment.IsDevelopment();
+                    logger.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning);
+                    logger.Enrich.FromLogContext();
+                    if (isDevelopment)
+                        logger.WriteTo.Console();
+                    logger.WriteTo.File(new JsonFormatter(),
+                        isDevelopment ? @".\log\log-.txt" : @"./log/log-.txt",
+                        rollingInterval: RollingInterval.Day,
+                        retainedFileCountLimit: null,
+                        rollOnFileSizeLimit: true);
+                }
+                )
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
