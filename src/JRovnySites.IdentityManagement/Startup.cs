7d5df899ff9a10930ac066cbccbe085506ff06fe
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-05 23:36:49 -0500

    Add IS4 events

--- src/JRovnySites.IdentityManagement/Startup.cs
+++ src/JRovnySites.IdentityManagement/Startup.cs
@@ -50,6 +50,11 @@ namespace JRovnySites.IdentityManagement
 
             var builder = services.AddIdentityServer(options =>
             {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+
                 // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
                 options.EmitStaticAudienceClaim = true;
             })
