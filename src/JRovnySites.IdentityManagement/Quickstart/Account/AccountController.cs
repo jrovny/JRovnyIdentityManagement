983d59814fa2a5c76cf64180fda3b3551d5d8ecc
Jordan Rovny <rovnyjordan@gmail.com>
2022-04-11 21:38:17 -0500

    Remove external login only check

--- src/JRovnySites.IdentityManagement/Quickstart/Account/AccountController.cs
+++ src/JRovnySites.IdentityManagement/Quickstart/Account/AccountController.cs
@@ -56,11 +56,11 @@ namespace IdentityServerHost.Quickstart.UI
             // build a model so we know what to show on the login page
             var vm = await BuildLoginViewModelAsync(returnUrl);
 
-            if (vm.IsExternalLoginOnly)
-            {
-                // we only have one option for logging in and it's an external provider
-                return RedirectToAction("Challenge", "External", new { scheme = vm.ExternalLoginScheme, returnUrl });
-            }
+            // if (vm.IsExternalLoginOnly)
+            // {
+            // we only have one option for logging in and it's an external provider
+            // return RedirectToAction("Challenge", "External", new { scheme = vm.ExternalLoginScheme, returnUrl });
+            // }
 
             return View(vm);
         }
